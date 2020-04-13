c518d89fcc0535cc6fc3dc21a81523b1577a2029
suhal vemu <vemusuhal@gmail.com>
2020-04-13 12:23:35 +0530

    Initial Commit

---
+++ BankingProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BankingProject
+//
+//  Created by Suhal Vemu on 13/04/20.
+//  Copyright © 2020 Suhal Vemu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
